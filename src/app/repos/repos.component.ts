0a96a5cef49b083c5757869284aafecf468fedc1
asder707 <abn.sebick@gmail.com>
2017-04-26 00:27:28 -0300

    Al seleccionar un repositorio este se muestra como el seleccionado

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
+
+
 @Component({
   selector: 'app-repos',
   templateUrl: './repos.component.html',
   styleUrls: ['./repos.component.css']
 })
+
 export class ReposComponent implements OnInit {
 
   repos  : any = [];
   loading : boolean = true;
-
+  selectedRepo : IRepo;
 
   constructor() { }
-
   ngOnInit() {
-
+  	this.selectedRepo = {
+  		name : "Angulabner", 
+  		description : "Proyecto de abner"
+  	}
   	setTimeout(()=>{
   		this.repos = [
   		{name: "Angulabner", description:"Proyecto de abner"},
@@ -22,7 +29,16 @@ export class ReposComponent implements OnInit {
   		{name: "app de escritorio", description:"Proyecto de escritorio muy generico"}
   	];
   	this.loading=false;
-  	},3000);
+  	},500);
   }
 
+  setRepo(repo){
+  	this.selectedRepo = repo; 
+  }
+
+}
+
+export interface IRepo{
+	name : string,
+	description : string
 }
