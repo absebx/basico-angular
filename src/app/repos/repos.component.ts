500a56ab55295f1dcd5b4d8abe0315d8e82b93d5
asder707 <abn.sebick@gmail.com>
2017-04-25 23:43:54 -0300

    se agrega aviso de carga para mientras se obtienen repositorios

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -8,16 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class ReposComponent implements OnInit {
 
   repos  : any = [];
+  loading : boolean = true;
 
 
   constructor() { }
 
   ngOnInit() {
-  	this.repos = [
+
+  	setTimeout(()=>{
+  		this.repos = [
   		{name: "Angulabner", description:"Proyecto de abner"},
   		{name: "Angular", description:"Un proyecto muy angular"},
   		{name: "app de escritorio", description:"Proyecto de escritorio muy generico"}
   	];
+  	this.loading=false;
+  	},3000);
   }
 
 }
